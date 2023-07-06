1068a61f21199dfdc2ef22e89ae94e489d3e63b9
Jacky Lui <jackymlui@live.com>
2023-07-06 14:56:05 +1000

    updated amplify to L2 construct

--- aws-infra/lib/aws-infra-stack.ts
+++ aws-infra/lib/aws-infra-stack.ts
@@ -1,7 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
-import * as amplify from 'aws-cdk-lib/aws-amplify';
+import * as amplify from '@aws-cdk/aws-amplify-alpha';
 import * as secrets_manager from 'aws-cdk-lib/aws-secretsmanager'
 
 
@@ -13,31 +13,15 @@ export class AwsInfraStack extends cdk.Stack {
       secretCompleteArn: "arn:aws:secretsmanager:ap-southeast-2:760383528016:secret:AWS-Amplify-Github-WuALmu"
     });
     const accessToken = accessTokenSecret.secretValueFromJson('GithubKey').unsafeUnwrap().toString()
-    console.log(accessToken)
 
-    const amplifyApp = new amplify.CfnApp(this, 'sample-react-app', {
-      name: "AWS-REACT-APP",
-      repository: "https://github.com/Recelis/AWS-Amplify-FullStackApp",
-      accessToken,
-    }
-
-    // {
-    //   sourceCodeProvider: new aws_amplify({
-    //     owner: "Recelis",
-    //     repository: "https://github.com/Recelis/AWS-Amplify-FullStackApp",
-    //     oauthToken: cdk.aws_secretsmanager
-    //       ("AWS-Amplify-Github", {
-    //       jsonField: "GithubKey"
-    //     })
-    //   })
-    // })
-    )
-    // const mainBranch = amplifyApp("main");
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsInfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const amplifyApp = new amplify.App(this, 'sample-react-app', {
+      appName: "AWS-REACT-APP",
+      sourceCodeProvider: new amplify.GitHubSourceCodeProvider({
+        owner: 'Recelis',
+        repository: "https://github.com/Recelis/AWS-Amplify-FullStackApp",
+        oauthToken: accessToken,
+      })
+    })
+    amplifyApp.addBranch('main')
   }
 }
